402428562b908e18a25879aa1a6b81e91ddc6fdc
cam-workmachine <rodriguezcm318@gmail.com>
2026-07-30 02:20:35 -0500

    asdfasdf

--- pull.py
+++ pull.py
@@ -3,8 +3,57 @@ import numpy as np
 import sodapy
 
 from sodapy import Socrata
-from requests.adapteres import HTTPAdapter
+from requests.adapters import HTTPAdapter
 from urllib3.util import Retry
 
+from datetime import datetime, timedelta
 
-client = Socrata()
+# set the client and authenticate
+client = Socrata("datacatalog.cookcountyil.gov", 
+                 "Q6WYPYmE8Ff40YoqUQJvDXJSR", 
+                 timeout=60)
+me_casearchive = "cjeq-bs86"
+
+# get metadata related to the me_casearchive
+meta = []
+meta = client.get_metadata(me_casearchive)
+
+# set time
+rightnow = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+today = datetime.today().strftime('%Y-%m-%d')
+yesterday = datetime.today() - timedelta(1)
+yesterday = yesterday.strftime('%Y-%m-%d')
+
+# set queries
+today_query = f"SELECT * WHERE heat_related = 'true' AND death_date BETWEEN '2026-01-01' AND '{today}'"
+yesterday_query = f"SELECT * WHERE heat_related = 'true' AND death_date BETWEEN '2026-01-01' AND '{yesterday}'"
+all_query = f"SELECT * WHERE heat_related = 'true'"
+
+# pull heat-related deaths from socrata into a dataframe
+today_download = client.get(me_casearchive, query=today_query, exclude_system_fields=False)
+yesterday_download = client.get(me_casearchive, query=yesterday_query, exclude_system_fields=False)
+all_download = client.get(me_casearchive, query=all_query, exclude_system_fields=False)
+
+# to dataframe
+today_df = pd.DataFrame(today_download)
+yesterday_df = pd.DataFrame(yesterday_download)
+all_df = pd.DataFrame(all_download)
+
+# to file
+today_df.to_csv('./data/today_heatdeaths.csv')
+yesterday_df.to_csv('./data/yesterday_heatdeaths.csv')
+all_df.to_csv('./data/all_heatdeaths.csv')
+
+# sanity check of full, queried dataset
+print("all heat deaths")
+print(len(all_df))
+
+print("heat deaths as of yesterday:")
+print(len(yesterday_df))
+
+print("heat deaths as of today:")
+print(len(today_df))
+
+# diff between
+print("diff between today and yesterday:")
+print(len(today_df) - len(yesterday_df))
---
+++ script.sh
@@ -0,0 +1,9 @@
+# run the pull.py file
+# Source - https://stackoverflow.com/a/4377147
+# Posted by Jean-Bernard Jansen, modified by community. See post 'Timeline' for change history
+# Retrieved 2026-07-30, License - CC BY-SA 3.0
+echo "Running... hopefully..."
+
+
+#!/bin/sh
+uv run pull.py
